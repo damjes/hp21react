e5e5966d957009dc32ee3eefb6838ed3489b0be2
Damjes <git@damj.es>
2024-03-29 16:15:16 +0100

    Działa wypisywanie liczb będących liczbami

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,64 @@
+import { useState } from "react";
 import Wyświetlacz from "./Wyswietlacz"
-
 import './App.sass'
 
+//type LiczbaWyświetlana = number
+type ZawartośćWyświetlacza = { napis: string, gdziePrzecinek: number }
+
+const długośćWyświetlacza = 12
+
 function App() {
+	const [poPrzecinku, _setPoPrzecinku] = useState(2)
+	const [wykładnicza, _setWykładnicza] = useState(false)
+	const [napisInputa, setNapisInputa] = useState('')
+	const liczba = parseFloat(napisInputa)
+
+	function zapiszMantyse(napisSurowy: string, miejsce: number): [string, number] {
+		const napisZeZnakiem = ((napisSurowy[0] === '-' ? '' : ' ') + napisSurowy)
+		const gdziePrzecinek = napisZeZnakiem.indexOf('.') - 1
+		const napis = napisZeZnakiem.replace('.', '').slice(0, miejsce - 1)
+
+		return [napis, gdziePrzecinek]
+	}
+
+	function ustalZawartośćWyświetlacza(liczba: number): ZawartośćWyświetlacza {
+		if(!Number.isFinite(liczba))
+			return {napis: ' Error', gdziePrzecinek: -1}
+
+		const abs = Math.abs(liczba)
+
+		if(abs < 1e-99) {
+			return {napis: ' ' + '0'.repeat(poPrzecinku+1), gdziePrzecinek: 1}
+		}
+
+		if(abs > 1e99) {
+			const znak = liczba > 0 ? ' ' : '-'
+			const mantysa = '9'.repeat(długośćWyświetlacza - 4)
+			const wykładnik = '99'
+			const napis = znak + mantysa + ' ' + wykładnik
+			return {napis, gdziePrzecinek: 1}
+		}
+		
+		if(!wykładnicza && abs > Math.pow(10, -poPrzecinku) && abs < Math.pow(10, długośćWyświetlacza - 2)){
+			const napisSurowy = liczba.toFixed(poPrzecinku)
+			const [napis, gdziePrzecinek] = zapiszMantyse(napisSurowy, długośćWyświetlacza)
+			return {napis, gdziePrzecinek}
+		}
+
+		const [mantysa, cecha] = liczba.toExponential(poPrzecinku).split('e')
+		const [mantysaZeZnakiem, gdziePrzecinek] = zapiszMantyse(mantysa, długośćWyświetlacza - 3)
+		const cechaZPoprawionymZnakiem = cecha.replace('+', ' ')
+		const napis = mantysaZeZnakiem.padEnd(długośćWyświetlacza - 3) + cechaZPoprawionymZnakiem
+
+		return {napis, gdziePrzecinek}
+	}
+
+	const {napis, gdziePrzecinek} = ustalZawartośćWyświetlacza(liczba)
+	
 	return <>
-		<Wyświetlacz napis="123456789012" przecinek={3} />
-		<Wyświetlacz napis=" Error -" przecinek={-1} />
+		<Wyświetlacz napis={napis} przecinek={gdziePrzecinek} długość={długośćWyświetlacza} />
+		<input type="text" name="napis" value={napisInputa} onChange={e => setNapisInputa(e.target.value)} />
+		<p>{liczba}</p>
 	</>
 }
 
--- src/Wyswietlacz.tsx
+++ src/Wyswietlacz.tsx
@@ -1,8 +1,6 @@
 import Cyfra from "./Cyfra"
 
-const długość = 12
-
-function Wyświetlacz({ napis, przecinek }: { napis: string, przecinek: number }) {
+function Wyświetlacz({ napis, przecinek, długość }: { napis: string, przecinek: number, długość: number}) {
     const cyfry = napis.padEnd(długość, ' ').slice(0, długość).split('')
     return <div className="wyswietlacz">
         {cyfry.map((cyfra, i) => <Cyfra key={i} cyfra={cyfra} kropka={i === przecinek} />)}
