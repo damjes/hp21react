ef97495b4b55602bc6e1284ffaec1c6e98f9aba7
Damjes <git@damj.es>
2024-03-29 04:54:50 +0100

    Upiększono tablicę segmentów w Cyfrze

--- src/Cyfra.tsx
+++ src/Cyfra.tsx
@@ -11,21 +11,22 @@ const styl_off = {
 }
 
 const segmenty: Record<string, boolean[]> = {
-	'0': [true, true, true, true, true, true, false],
-	'1': [false, true, true, false, false, false, false],
-	'2': [true, true, false, true, true, false, true],
-	'3': [true, true, true, true, false, false, true],
-	'4': [false, true, true, false, false, true, true],
-	'5': [true, false, true, true, false, true, true],
-	'6': [true, false, true, true, true, true, true],
-	'7': [true, true, true, false, false, false, false],
-	'8': [true, true, true, true, true, true, true],
-	'9': [true, true, true, true, false, true, true],
+	//      A      B      C      D      E      F      G
+	'0': [true , true , true , true , true , true , false],
+	'1': [false, true , true , false, false, false, false],
+	'2': [true , true , false, true , true , false, true ],
+	'3': [true , true , true , true , false, false, true ],
+	'4': [false, true , true , false, false, true , true ],
+	'5': [true , false, true , true , false, true , true ],
+	'6': [true , false, true , true , true , true , true ],
+	'7': [true , true , true , false, false, false, false],
+	'8': [true , true , true , true , true , true , true ],
+	'9': [true , true , true , true , false, true , true ],
 	' ': [false, false, false, false, false, false, false],
-	'-': [false, false, false, false, false, false, true],
-	'E': [true, false, false, true, true, true, true],
-	'r': [false, false, false, false, true, false, true],
-	'o': [false, false, true, true, true, false, true],
+	'-': [false, false, false, false, false, false, true ],
+	'E': [true , false, false, true , true , true , true ],
+	'r': [false, false, false, false, true , false, true ],
+	'o': [false, false, true , true , true , false, true ],
 }
 
 const kształty = [
